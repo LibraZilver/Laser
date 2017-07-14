87a32ad730043563078627025e34dae4fd3b0d09
watcharaChe <watchara.che@gmail.com>
2017-07-14 17:09:28 +0700

    second commit

--- app/src/main/java/com/example/watcharac/laser/MainActivity.java
+++ app/src/main/java/com/example/watcharac/laser/MainActivity.java
@@ -58,8 +58,8 @@ public class MainActivity extends Activity {
 //                String phone = string.replaceAll("[^\\d\\w]", "");
 //                String phone = string.replaceAll("[^([a-zA-Z]{2}+[0-9]-[0-9]{7}-[0-9]{2})]", "");
 
-//                String phone = string.replaceAll("[^(^(\\a{2})+([0-9-])*)]", "");
-                String phone = string.replaceAll("[A-Z]{4}\\d+", "");
+                String phone = string.replaceAll("[^(^(\\a{2})+([0-9-])*)]", "");
+//                String phone = string.replaceAll("[a-zA-Z]{2}\\d{6}", "");
 
 //                int cursorPosition = ET.getSelectionStart();
 //                if (cursorPosition > 1) {
