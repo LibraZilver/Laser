b84d71ab3a7e269599a8642c693d5209bb4b59a8
watcharaChe <watchara.che@gmail.com>
2017-07-17 15:43:28 +0700

    OK

--- app/src/main/java/com/example/watcharac/laser/LaserNumberFilter.java
+++ app/src/main/java/com/example/watcharac/laser/LaserNumberFilter.java
@@ -29,32 +29,17 @@ public class LaserNumberFilter extends NumberKeyListener {
     public CharSequence filter(CharSequence source, int start, int end,
                                Spanned dest, int dstart, int dend) {
 
-
         if (dend == dstart) {
-
 
             String destTxt = dest.toString();
             String resultingTxt = destTxt.substring(0, dstart) + source.subSequence(start, end) + destTxt.substring(dend);
 
             // Laser number must match xxx-xxxxxxx-xx
-//            if (!resultingTxt.matches("^[a-zA-Z]{1,1}([a-zA-Z]{1,1}(\\d{1,1}(\\-(\\d{1,1}(\\d{1,1}(\\d{1,1}(\\d{1,1}(\\d{1,1}(\\d{1,1}(\\d{1,1}(\\-(\\d{1,1}(\\d{1,1}?)?)?)?)?)?)?)?)?)?)?)?)?)?")) {
-//            if (!resultingTxt.matches("^[a-zA-Z]{1,1}([a-zA-Z]{1,1}([0-9-]{1,12}?)?)?")) {
-
             if (!resultingTxt.matches("^[a-zA-Z]{1,1}([a-zA-Z]{1,1}([0-9-]{1,12}?)?)?")) {
-//            if (!resultingTxt.matches("^[a-zA-Z]{1,2}([0-9-]{1,12}?)?")) {
-                if(destTxt.matches("[0-9-]{1,12}")){
-                    if(!source.equals("")&&source.toString().matches("[a-zA-Z]")){
-                    }
-                    else
-                        return "";
-                }
-                else
+                if (!(destTxt.matches("[0-9-]{1,12}") && source.toString().matches("[a-zA-Z]"))) {
                     return "";
-//                if (!(destTxt.matches("\\d-") && Character.isLetter(source.charAt(0))))
-//                    return "";
+                }
             }
-
-
         }
 
         return null;
